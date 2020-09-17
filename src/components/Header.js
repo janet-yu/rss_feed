4adc4a9662c0d768b14fa983d52c5775e9fe7853
janet-yu <janettyu99@gmail.com>
2020-09-17 10:01:28 -0700

    chore: add event arg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import Searchbar from './Searchbar';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Searchbar from "./Searchbar";
+import { Link } from "react-router-dom";
 
 function Header(props) {
   return (
     <header>
-        <div className="container">
-          <a href='' onClick={() => { document.location.reload(true)} }><h1 id="site-heading">RSS FEED</h1></a>
-          <span id="header-subtitle">Get your daily news all in one place.</span>
-          <Searchbar onSearch={props.onSearch} onSelect={props.onSelect} onClear={props.onClear}/>
-        </div>
+      <div className="container">
+        <a
+          href=""
+          onClick={(e) => {
+            document.location.reload(true);
+          }}
+        >
+          <h1 id="site-heading">RSS FEED</h1>
+        </a>
+        <span id="header-subtitle">Get your daily news all in one place.</span>
+        <Searchbar
+          onSearch={props.onSearch}
+          onSelect={props.onSelect}
+          onClear={props.onClear}
+        />
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header;
